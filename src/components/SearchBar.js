4497d276b7d07c4824cc951bedc26325c3e9ee44
markhorvath <markhorvath7@gmail.com>
2020-01-24 19:00:34 -0500

    notes on special prop events, setting state in input

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,13 +1,24 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+    //e for event
+    // onInputChange(e) {
+    //     e.target.value;
+    // }
+    state = { term: '' };
+
+    //we leave the paranthesis off because otherwise it's automatically called whenever the component is rendered
+    //by leaving them off we are passing a reference to this function to the input element so that the input can call it in the future
+    //onChange is a special prop name for input elements.  similar to onClick and onSubmit.
+    //<input type="text" onChange={(e) => console.log(e.target.value)} /> use this format usually if it's just one line of code for the event handler
     render() {
         return (
             <div className="ui segment">
                 <form className="ui form">
                     <div className="field">
                     <label>Image Search</label>
-                    <input type="text" />
+
+                    <input type="text" value={this.state.term} onChange={(e) => this.setState({ term: e.target.value })} />
                     </div>
                 </form>
             </div>
